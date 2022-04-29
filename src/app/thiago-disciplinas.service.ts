fa0bf892a76ec63dc9002aa0af15fb0769f9d635
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-04-29 20:50:10 -0300

    1 file modified, 1 added

--- src/app/thiago-disciplinas.service.ts
+++ src/app/thiago-disciplinas.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
 
+interface Materias{
+  title: string;
+}
+
 @Injectable()
 export class ThiagoDisciplinasService {
+  list: Array<Materias> = [];
 
   constructor() { }
+
+  getMateria(){
+    return this.list;
+  }
+
+  addMateria(title:string){
+    this.list.push({title});
+  }
+
+  removeMateria(index:number){
+    this.list.splice(index, 1)
+  }
 
 }
