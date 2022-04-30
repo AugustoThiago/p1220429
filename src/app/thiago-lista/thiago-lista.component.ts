bd8cc2a53f44071f81fdeadb45665f1fbf014c1e
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-04-29 21:46:51 -0300

    2 files modified

--- src/app/thiago-lista/thiago-lista.component.ts
+++ src/app/thiago-lista/thiago-lista.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import  { ThiagoDisciplinasService } from '../thiago-disciplinas.service';
 
+interface Materias{
+  nome: string;
+}
 
 @Component({
   selector: 'app-thiago-lista',
@@ -8,8 +11,21 @@ import  { ThiagoDisciplinasService } from '../thiago-disciplinas.service';
   styleUrls: ['./thiago-lista.component.css']
 })
 export class ThiagoListaComponent implements OnInit {
+  materias: Array<Materias> = [];
   
   constructor() { }
+
+  getMateria(){
+    return this.materias;
+  }
+
+  addMateria(nome:string){
+    this.materias.push({nome});
+  }
+
+  removeMateria(index:number){
+    this.materias.splice(index, 1)
+  }
 
   ngOnInit() {
   }
